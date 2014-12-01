b2c5f3976028ca4171420893c9736b2c3b7f0d97
pablodicosta <pablo.diaz.acosta@gmail.com>
2014-12-01 18:54:36 -0300

    Added exercise 5

--- server.js
+++ server.js
@@ -2,6 +2,15 @@ var express = require('express'),
 	server = express();
 
 server.use(express.static(__dirname + '/apps'));
+
+server.get('/rest/date', function (req, res) {
+	setTimeout(function () {
+		res.json ({
+			currentDate: new Date()
+		});
+	}, 3000);
+});
+
 server.listen(3000);
 
 console.log('Server started...');
